948ac0b0dd3322beb5b692ee3e232b5de8972e2d
Rohit <rohit@translense.com>
2023-02-28 16:56:41 +0530

    user auth

---
+++ src/services/user/user.services.js
@@ -0,0 +1,16 @@
+const { UserModel } = require('../../models');
+
+const getUserByEmail = async ({ email }) => UserModel.findOne({ email });
+const addUser = async ({
+  email, password, fullName, OTP,
+}) => UserModel.create({
+  email,
+  password,
+  fullName,
+  OTP,
+});
+
+module.exports = {
+  getUserByEmail,
+  addUser,
+};
